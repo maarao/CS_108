b390fdfccecaa09aec54a0ce59ed1bf284e35e17
zenM49 <58347499+maarao@users.noreply.github.com>
2022-09-26 21:48:04 -0400

    Added notes to Midterm_Notes

--- Midterm_1_Review.py
+++ Midterm_1_Review.py
@@ -2,15 +2,15 @@
 # parts of a dataclass
 
 # Type-casts to int after getting result
-'''1 // .5 #= 2'''
+"""1 // .5 #="""
 
-#== False # Compares the unicode characters
-'''“1” > “200”'''
+# == False # Compares the unicode characters
+"""“1” > “200”"""
 
 # escape characters
 '''print('I\'ll do great on this exam')'''
 
-#truthiness
+# truthiness
 '''
 x = 4
 print(x == 5 or 10)
@@ -19,4 +19,37 @@ or 10 means 10 == True so the entire thing print to "True"
 False == False but "False" == True
 
 if something is empty / 0 / False       == False
+'''
+
+# mutability
+'''
+lst1 = [1, 2, 3]
+lst2 = [1, 2, 3]
+
+lst2 = lst1.append(1)
+lst2 == None
+lst1 = [1, 2, 3, 1]
+
+LISTS ARE MUTABLE
+DATACLASSES ARE MUTABLE
+PRIMITIVES ARE NOT MUTABLE -> MUST BE ASSIGNED TO A NEW VARIABLE
+'''
+
+# datclasses
+'''
+from datclasses import dataclass
+
+@datclass
+'''
+
+# check to make sure its calling a function w/ parenthesis() and not a variable
+# Ex: Function ->print() vs print <- Variable
+
+# designer will get documentation
+
+# imports
+'''
+from dataclasses import dataclass
+from bakery import assert_equal
+from random import choice
 '''
