046cf71f708de7d6af0e44716866a8d870cefa66
zenM49 <58347499+maarao@users.noreply.github.com>
2022-10-03 10:13:28 -0400

    From Lecture 10/3/22

---
+++ For_Loops.py
@@ -0,0 +1,17 @@
+def increment(alist):
+    incremented = []
+    for a in alist:
+        incremented.append(a * 2)
+
+    return incremented
+print(increment([0, 1, 2]))
+
+base = [0, 1, 2]
+
+print("0123")
+for i in base:
+    print(i+1, end="")
+    for j in base:
+        print((i+1) * (j+1), end="")
+    print("")
+
